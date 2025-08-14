4af667c108ba8f9f4eda134e39879477f585be40
sardorbek-3226 <ibrohimovs@gmail.com>
2025-08-14 10:30:34 +0300

    seventh commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Images from "./component/Image";
+import Rasm from "./component/Rasm.jsx";
 import CartModal from "./component/CartModal";
 
 const App = () => {
   return (
     <>
       <h1 style={{textAlign:"center", fontSize:"45px"}}>Desert</h1>
-      <Images />
+      <Rasm />
       <CartModal />
     </>
   );
